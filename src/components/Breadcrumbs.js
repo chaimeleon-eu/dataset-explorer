df08fa5f252135f2f9762cb0945d11e3234af029
Andy S Alic (asalic) <asalic@users.noreply.github.com>
2022-02-16 11:06:31 +0100

    Navigation inside the page fix #12

---
+++ src/components/Breadcrumbs.js
@@ -0,0 +1,21 @@
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from 'react' ;
+import { Breadcrumb } from 'react-bootstrap';
+
+function Breadcrumbs(props) {
+   let location = useLocation();
+
+   useEffect(() => {
+         console.log(`You changed the page to: ${location.pathname}`);
+   }, [location])
+
+  return (<Breadcrumb>
+    <Breadcrumb.Item linkAs={Link} linkProps={{ to: "/" }}>Home</Breadcrumb.Item>
+    {props.elems.map((value, index) => {
+          return <Breadcrumb.Item {...(value.active ? {active: true} : {})}
+            linkAs={Link} linkProps={{ to: value.link }}>{value.text}</Breadcrumb.Item>;
+        })}
+  </Breadcrumb>);
+}
+
+export default Breadcrumbs;
