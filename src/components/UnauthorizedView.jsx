b29b68f765b65d7b2f1fd3538724b710e829794e
Andy S Alic (asalic) <asalic@users.noreply.github.com>
2022-02-16 11:12:28 +0100

    unauthorized view for protected parts

---
+++ src/components/UnauthorizedView.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function UnauthorizedView(props) {
+
+  return (
+    <h4 className="m-2">Please log in to see this page or return to the <Link to="/">home</Link> page.</h4>
+  )
+
+}
+
+export default UnauthorizedView;
