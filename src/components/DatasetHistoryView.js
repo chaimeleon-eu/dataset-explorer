9a03ca971b5e91fbbe1a6c5d4a5193525e79231e
Andy S Alic (asalic) <asalic@users.noreply.github.com>
2022-12-06 11:01:19 +0100

    History traces pagination support fix #72

--- src/components/DatasetHistoryView.js
+++ src/components/DatasetHistoryView.js
@@ -2,10 +2,14 @@ import { useState, useMemo, useEffect } from "react";
 import { ListGroup, Button, InputGroup, FormControl, Table as BTable, Container, Row, Col} from 'react-bootstrap';
 import { useTable, useRowSelect } from 'react-table';
 import { useKeycloak } from '@react-keycloak/web';
+import {
+  useSearchParams,
+} from 'react-router-dom';
 
 import Message from "../model/Message.js";
 import DataFilterView from "./DataFilterView.js";
 import LoadingView from "./LoadingView";
+import Config from "../config.json";
 
 const NoDataConst = props => (
   <div>No data.</div>
@@ -55,14 +59,18 @@ function TableComponent({ columns, data }) {
 
 
 function DatasetHistoryView(props) {
-
-
+  const [searchParams, setSearchParams] = useSearchParams();
+  const defSkip = searchParams.get("skipStudies");
+  const defLimit = searchParams.get("limitStudies");
+  const skip = defSkip === null ? 0 : Number(defSkip);
+  const limit = defLimit === null ? Config.defaultLimitTraces : Number(defLimit);
 
   const [data, setData] = useState({
     isLoading: false,
     isLoaded: false,
     traces: [],
     tracesFiltered: [],
+    totalTracesCnt: 0,
     error: null,
     status: -1
   });
@@ -89,12 +97,16 @@ function DatasetHistoryView(props) {
            });
         if (props.keycloakReady && keycloak.authenticated) {
 
-              props.dataManager.getTracesDataset(keycloak.token, props.datasetId)
+              props.dataManager.getTracesDataset(keycloak.token, props.datasetId, skip, limit)
                 .then(
                   (xhr) => {
+                    let totalTracesCnt = 0;
                     let traces  = [];
                     const rawTraces = JSON.parse(xhr.response).traces;
                     for (let rt of rawTraces) {
+                      console.log(rt.countAllTraces);
+                      if (rt.countAllTraces !== undefined)
+                        totalTracesCnt += rt.countAllTraces;
                       for (let t of rt.traces) {
                           let d = new Date(t.timestamp);
                           //console.log(t.timestamp);
@@ -105,7 +117,7 @@ function DatasetHistoryView(props) {
                     }
                     traces.sort((a,b) => b.created - a.created);
                     setData( prevValues => {
-                       return { ...prevValues, isLoaded: true, traces: traces, isLoading: false, status: xhr.status, error: null }
+                       return { ...prevValues, totalTracesCnt, isLoaded: true, traces: traces, isLoading: false, status: xhr.status, error: null }
                        });
                   },
                   (xhr) => {
@@ -126,7 +138,7 @@ function DatasetHistoryView(props) {
                       }
                     }
                     setData( prevValues => {
-                       return { ...prevValues, isLoaded: true, traces: [], isLoading: false,
+                       return { ...prevValues, totalTracesCnt: 0,  isLoaded: true, traces: [], isLoading: false,
                           status: xhr.status, error: text }
                        });
                     props.postMessage(new Message(Message.ERROR, title, text));
@@ -134,7 +146,7 @@ function DatasetHistoryView(props) {
             }
 
     }, //1000);},
-    [props.keycloakReady, keycloak.authenticated]);
+    [props.keycloakReady, keycloak.authenticated,  searchParams]);
 
   const columns = useMemo(
     () => [
@@ -166,13 +178,50 @@ function DatasetHistoryView(props) {
     if (data.isLoading) {
       return <LoadingView what="dataset history" />;
     }
+    const lastPage = Number(data.totalTracesCnt) % Number(limit) === 0 ? 0 : 1;
+    let numPages = Math.floor(Number(data.totalTracesCnt) / Number(limit)) + lastPage;
+    if (numPages === 0)
+      numPages = 1;
+  
+    const page = Number(skip) / Number(limit) + 1;
     return (
         <Container fluid>
           <Row>
             <Col lg={3} md={12}>
-              <DataFilterView traces={data.traces} updFilteredData={updFilteredData} keycloakReady={props.keycloakReady} dataManager={props.dataManager} postMessage={props.postMessage}/>
+              <DataFilterView traces={data.traces} updFilteredData={updFilteredData} 
+                keycloakReady={props.keycloakReady} dataManager={props.dataManager} 
+                postMessage={props.postMessage}/>
             </Col>
-            <Col lg={9} md={12}><TableComponent columns={columns} data={data.tracesFiltered} NoDataComponent={NoDataConst} /></Col>
+            <Col lg={9} md={12} className="d-flex flex-column">
+              <TableComponent columns={columns} data={data.tracesFiltered} NoDataComponent={NoDataConst} />
+              <div className="w-100  d-flex  justify-content-center align-self-end" >
+                <Button className="position-relative me-4" disabled={page === 1 ? true : false}
+                  onClick={(e) => {
+                    setSearchParams(prevValues => {
+                      return { ...prevValues, 
+                      skipStudies: (skip - limit),
+                      limitStudies: limit };
+                      });
+                    // setSkipLimit(prevValues => {
+                    //   return { ...prevValues, skip, limit };
+                    // });
+                  }
+                  }>Previous</Button>
+                <Button className="position-relative me-4"  disabled={page === numPages ? true : false}
+                  onClick={(e) => {
+                    setSearchParams(prevValues => {
+                      return { ...prevValues, 
+                      skipStudies: (skip + limit),
+                      limitStudies: limit };
+                      });
+                    // setSkipLimit(prevValues => {
+                    //   return { ...prevValues, skip, limit };
+                    // });
+                  }
+                    }>Next</Button>
+                <span>Page <b>{page}</b> of <b>{numPages}</b></span>
+              </div>
+            </Col>
           </Row>
         </Container>
     );
