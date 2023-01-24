268a8cdff178dc77c5b36b92086ac0442ec9efb1
Andy S Alic (asalic) <asalic@users.noreply.github.com>
2023-01-24 10:09:58 +0100

    smaller font for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,9 @@ import Config from "../config.json";
 function Footer(props) {
 
     return (
-        <div className="d-flex justify-content-between w-100 p-1 text-black bg-light bg-gradient mt-4">
-            <span className="ms-2"><img src={Config["basename"] + "/icons/eu.svg"} style={{height:"1em"}}/><b className="ms-2">CHAIMELEON Project</b>, DOI <a href="https://doi.org/10.3030/952172">10.3030/952172</a></span>
+        <div className="d-flex justify-content-between w-100 p-1 text-black bg-light bg-gradient mt-4 " style={{"font-size":"0.75em"}}>
+            <span className="ms-2"><img src={Config["basename"] + "/icons/eu.svg"} 
+                style={{height:"0.75em"}}/><b className="ms-2">CHAIMELEON Project</b>, DOI <a href="https://doi.org/10.3030/952172">10.3030/952172</a></span>
             <span>Copyright© <a href="https://www.upv.es/en">UPV</a> 2020-2024</span>
             <span className="me-2">Powered by <a href="https://reactjs.org/">React</a> & <a href="https://react-bootstrap.github.io/">React Bootstrap</a></span>
         </div>
