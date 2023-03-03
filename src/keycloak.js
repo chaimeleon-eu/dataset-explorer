d7fa182fe1135f630840bc828131f1f31b3e0504
Andy S Alic (asalic) <asalic@users.noreply.github.com>
2023-03-03 13:47:08 +0100

    Update keycloak JS config/initOptions fix asalic/dataset-explorer#84

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,6 +1,8 @@
 import Keycloak from 'keycloak-js';
 import Config from "./config.json";
 
-const keycloak = new Keycloak(Config.keycloak);
+const keycloak = new Keycloak(Config.keycloak.config);
+
+keycloak.init(Config.keycloak.initOptions);
 
 export default keycloak;
