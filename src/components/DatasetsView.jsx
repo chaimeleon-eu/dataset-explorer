8c3b2224200f08e61292063827e8397fea7a54f9
Andy S Alic <>
2024-02-14 12:23:08 +0100

    Pagination not working list datasets fix chaimeleon-eu/dataset-explorer#126, fix asalic/dataset-explorer#105

--- src/components/DatasetsView.jsx
+++ src/components/DatasetsView.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect, useCallback }from 'react';
 import ReactDOM from 'react-dom';
-import { Button, InputGroup, FormControl, Table as BTable, Container, Row, Col} from 'react-bootstrap';
+import { Button, InputGroup, FormControl, Table as BTable, Container, Row, Col } from 'react-bootstrap';
 import { Search as SearchIc, FilePlus as FilePlusIc } from "react-bootstrap-icons";
 import { useTable, useRowSelect } from 'react-table';
 import { useKeycloak } from '@react-keycloak/web';
@@ -12,6 +12,7 @@ import DatasetsSearch from  "./DatasetsSearch";
 import DatasetsMainTable from "./DatasetsMainTable";
 import Config from "../config.json";
 import DatasetsFiltering from './filter/DatasetsFiltering.jsx';
+import PaginationFooter from './PaginationFooter.jsx';
 
 function getSortDirectionDesc(searchParam, sortBy) {
   if (!sortBy) {
@@ -43,7 +44,7 @@ function DatasetsView (props) {
 
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
   // const [skip, setSkip] = useState(0);
   // const [limit, setLimit] = useState(Config.defaultLimitDatasets);
 
@@ -63,6 +64,11 @@ function DatasetsView (props) {
   const sortDirection = getSortDirectionDesc(searchParams.get("sortDirection"), searchParams.get("sortBy") ?? "creationDate");
   const skip = searchParams.get("skip") ? Number(searchParams.get("skip")) : 0;
   const limit = searchParams.get("limit") ? Number(searchParams.get("skip")) : Config.defaultLimitDatasets;
+  
+  const onSkipChange = useCallback((skip) => {
+    updSearchParams({skip: skip === 0 ? null : skip});
+  }, [skip, updSearchParams]);
+
 
   //console.log(`searchString is ${searchString}`);
   // const setSearchString = (newVal) => {
@@ -106,12 +112,12 @@ function DatasetsView (props) {
                       (xhr) => {
                         setIsLoaded(true);
                         const d = JSON.parse(xhr.response);
-                        console.log(d);
+                        //console.log(d);
                         setData(d);
                       },
                       (xhr) => {
                         setIsLoaded(true);
-                        console.log(xhr);
+                        //console.log(xhr);
                         let title = null;
                         let text = null;
                         if (!xhr.responseText) {
@@ -128,7 +134,6 @@ function DatasetsView (props) {
 
         }, //1000);},
         [props.keycloakReady, searchParams, sortBy, sortDirection, skip, limit, searchString]);
-
       return (
         <Container fluid>
           <Row>
@@ -148,10 +153,12 @@ function DatasetsView (props) {
                 }}
                 updSearchParams={updSearchParams}
                 />
-                <div className="w-100" >
+                <div className="d-flex flex-row justify-content-center w-100" >
+                  {/*
                   <Button variant="link" className="position-relative start-50 me-4" disabled={skip === 0 ? true : false} onClick={(e) => updSearchParams({skip: skip - limit})}>&lt; Previous</Button>
                   <Button variant="link" className="position-relative start-50"  disabled={data?.list?.length <= limit ? true : false} onClick={(e) => updSearchParams({skip: skip + limit})}>Next &gt;</Button>
-                  {/* <TableNavigationPages skip={skip} limit={limit} total={data} */}
+                  TableNavigationPages skip={skip} limit={limit} total={data} */}
+                  <PaginationFooter skip={skip} limit={limit} total={data?.total} onSkipChange={onSkipChange} className="" />
                 </div>
               {/* </Col> */}
           </Row>
---
+++ src/components/PaginationFooter.jsx
@@ -0,0 +1,42 @@
+import { Pagination } from 'react-bootstrap';
+import { useCallback } from "react";
+
+const PREVIOUS_PAGE = 0;
+const NEXT_PAGE = -1
+
+
+function PaginationFooter({skip, limit, total, onSkipChange}) {
+    const onClickPg = useCallback((pg) => {
+        onSkipChange(limit * (pg - 1));
+    }, [skip, limit]);
+
+    const onClickPrevious = useCallback(() => {
+        onSkipChange(skip - limit);
+    }, [skip, limit]);
+
+    const onClickNext = useCallback(() => {
+        onSkipChange(skip + limit);
+    }, [skip, limit]);
+
+    
+    let items = [];
+    if (total !== 0  && limit !== 0) {
+
+        const numPages = Math.trunc(total / limit) + (total % limit === 0 ? 0 : 1);
+        console.log(numPages);
+        const activePg = 1 + Math.trunc(skip / limit);
+        items.push(<Pagination.Prev  onClick={onClickPrevious} disabled={activePg === 1} />);
+        for (let pg=1; pg<=numPages; ++pg) {
+            items.push(
+                <Pagination.Item onClick={() => onClickPg(pg)} key={pg} active={pg === activePg}>
+                {pg}
+                </Pagination.Item>
+            );
+        }
+        items.push(<Pagination.Next  onClick={onClickNext} disabled={activePg === numPages} />);
+    }
+
+    return <Pagination size="sm">{items}</Pagination>;
+}
+
+export default PaginationFooter;
